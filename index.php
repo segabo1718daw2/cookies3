e943a4a0277e92c2b06360b5a42a8fd7d7a27079
segabo <segabo1718daw2@gmail.com>
2018-04-13 19:04:30 +0200

    cookies3 commit 1

---
+++ index.php
@@ -0,0 +1,17 @@
+<html> 
+    <head> 
+        <meta charset="UTF­8"> 
+        <title>Desplegant una aplicació PHP sobre Heroku</title> 
+    </head> 
+    <body> 
+        <?php 
+            echo "Afegint Cookies<br>"; 
+            echo "Sóc el desenvolupador segabo1718daw2"; 
+            setcookie("nom", "Jordi Pons", time()+3600, "/", "", 0); 
+            setcookie("edat", "21", time()+3600, "/", "", 0); 
+            setcookie("cicle", "DAW", time()+3600, "/", "", 0); 
+            setcookie("curs", "2n", time()+3600, "/", "", 0); 
+            setcookie("any", "2017­2018", time()+3600, "/", "", 0);            
+        ?>  
+    </body>    
+</html>
